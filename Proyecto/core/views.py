12daee570c510ca646ec3c3211e8c6d2e46665e1
Jonathan Bustos <jonathan.bustos1@claro.com.ar>
2024-01-14 21:30:32 -0300

    Fin_Entrega

--- Proyecto/core/views.py
+++ Proyecto/core/views.py
@@ -23,3 +23,41 @@ def profesor_create(request):
     else:
         form = forms.ProfesorForm()
     return render(request, "core/profesor_create.html",{"form": form})
+
+
+
+
+def estudiante_list(request):
+    consulta = models.Estudiante.objects.all()
+    contexto = {"estudiantes": consulta}
+    return render(request, "core/estudiante_list.html",contexto)
+
+
+def estudiante_create(request):
+    if request.method == "POST":
+        form = forms.EstudianteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("estudiante_list")
+    else:
+        form = forms.EstudianteForm()
+    return render(request, "core/estudiante_create.html",{"form": form})
+
+
+
+
+def curso_list(request):
+    consulta = models.Curso.objects.all()
+    contexto = {"cursos": consulta}
+    return render(request, "core/curso_list.html",contexto)
+
+
+def curso_create(request):
+    if request.method == "POST":
+        form = forms.CursoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("curso_list")
+    else:
+        form = forms.CursoForm()
+    return render(request, "core/curso_create.html",{"form": form})
